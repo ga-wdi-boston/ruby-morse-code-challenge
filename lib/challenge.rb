8e3b301398dee452b2543fb16d08c33554ac5c6f
rickdean <rickdean@outlook.com>
2016-12-23 09:25:56 -0500

    Commit response to challenge
    Response to ruby more code challenge (incomplete)

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -15,8 +15,74 @@ MORSE_CODE = {
 }.freeze
 ## DO NOT CHANGE ANYTHING ABOVE THIS LINE
 
+
+## need to create new array with commas
 def decode_morse(morse_code)
-  # Your code here
+ morse_code.each do |value|
+case morse_code
+when i = ''
+  puts ','
+
+## iterate through new array
+
+
+  when i == '.-'
+    puts 'A'
+  when i == '-...'
+    puts 'B'
+  when i == '-.-.'
+    puts 'C'
+  when i == '-..'
+    puts 'D'
+  when i == '.'
+    puts 'E'
+  when i == '..-.'
+    puts 'F'
+  when i == '--.'
+    puts 'G'
+  when i == '....'
+    puts 'H'
+  when i == '..'
+    puts 'I'
+  when i == '.---'
+    puts 'J'
+  when i == '-.-'
+    puts 'K'
+  when i == '-..'
+    puts 'L'
+  when i == '-..'
+    puts 'M'
+  when i == '-..'
+    puts 'N'
+  when i == '-..'
+    puts 'O'
+  when i == '-..'
+    puts 'P'
+  when i == '-..'
+    puts 'Q'
+  when i == '-..'
+    puts 'R'
+  when i == '-..'
+    puts 'S'
+  when i == '-..'
+    puts 'T'
+  when i == '-..'
+    puts 'U'
+  when i == '-..'
+    puts 'V'
+  when i == '-..'
+    puts 'W'
+  when i == '-..'
+    puts 'X'
+  when i == '-..'
+    puts 'Y'
+  when i == '-..'
+    puts 'Z'
+
+##  Did not have time to finish before 30 minutes....
+
+
+
 end
 
 def parse_bits(bits)
