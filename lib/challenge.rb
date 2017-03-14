107f8d807d657f73fdd53b5e84e63e1585c9997d
cathyob <cathy.obrien09@gmail.com>
2017-03-14 18:55:29 -0400

    Ruby Practice - Morse Code Translator
    A lot of googling to make the linter stop yelling at me for preferred
    syntax

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -3,7 +3,19 @@
 require_relative './morse_code'
 
 def decode_morse(morse_code)
-  # Your code here
+  return_string = ''
+  # split on 3 spaces to make words, after splitting into words,
+  # iterate through each word and get each letter
+  morse_code.split('   ').each do |current_word|
+    # if we already have a word add a space before the next one
+    return_string += ' ' unless return_string == ''
+    # split on 1 space to make letters, after splitting take each letter
+    # and translate using morse_code.rb
+    current_word.split(' ').each do |current_letter|
+      return_string += MORSE_CODE[current_letter] || '*'
+    end
+  end
+  return_string
 end
 
 def parse_bits(bits)
